784265ea4624603c39eaeedbfc7cb14f6cff793d
Ashley Hayes <ashleymh060504@gmail.com>
2025-02-11 14:16:07 -0500

    Added pkg.json at root level, switched routes from App to main, created blank footer, header Schedule and Tasks components, created Dashboard and Home pages, moved Login and Register from pas to components.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Home from './Home.jsx';
+import Dashboard from './Dashboard.jsx';
+import Social from './Social.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const BrowserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: '/social',
+        element: <Social />,
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={BrowserRouter}></RouterProvider>
   </StrictMode>,
 )
