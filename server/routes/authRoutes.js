4b9980ffb1c1ea5258a3f6d316f965363e85b8ae
Ashley Hayes <ashleymh060504@gmail.com>
2025-02-14 14:17:09 -0500

    trying to fix import of authController in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authcontroller";
+import { registerUser, loginUser } from "../controllers/authController";
 
 const router = express.Router();
 
