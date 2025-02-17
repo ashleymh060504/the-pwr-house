b6213f6458c4c7594975006992ba431778afc3c4
Ashley Hayes <ashleymh060504@gmail.com>
2025-02-17 15:03:34 -0500

    corrected update to put

--- server/routes/events.js
+++ server/routes/events.js
@@ -4,10 +4,9 @@ import { verifyToken } from "../middleware/auth.js";  // Ensures only logged-in 
 
 const router = express.Router();
 
-// Create a new event
 router.post("/", verifyToken, createEvent);
 router.get("/", verifyToken, getUserEvents);
 router.delete("/:id", verifyToken, deleteEvent);
-router.update("/:id", verifyToken, updateEvent);
+router.put("/:id", verifyToken, updateEvent);
 
 export default router;
