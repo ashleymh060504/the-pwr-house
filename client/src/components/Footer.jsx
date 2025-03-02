24618c0e236f6d862d754c69e17151ba841d9d1b
Ashley Hayes <ashleymh060504@gmail.com>
2025-03-02 14:40:23 -0500

    worked on home page design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import React from "react";
+import '../styles/index.css';
+import githubLogo from '../assets/github-mark-white.png';
+
 function Footer() {
   return (
     <footer>
       <p>&copy; 2025 Ashley Hayes</p>
+      <a href="https://github.com/ashleymh060504" target="_blank" rel="noopener noreferrer">
+                <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+      </a>
+      <p>Questions? Email: ashleymh060504@gmail.com</p>
     </footer>
   );
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,12 +6,14 @@ const Welcome = () => {
         <div className= "img-fluid background-image" >
             <section>
                 <h1>Welcome.</h1>
+                <br></br>
                 <p>This site is for all of you Moms and Dads out there, working from home and facing the daily challenge of balancing both work and the needs of your child(ren).</p>
                 <p>Create an account today to get organized, separating your "Work" and "Home" tasks and calendar, while keeping it all in one place!</p>
             </section>
             <br></br>
             <section>
                 <h4>COMING SOON!</h4>
+                <br></br>
                 <p>Connect with other PWR's by visiting PWR Connect!</p>
                 <p>Get advice, vent your frustrations, share tips and tricks, etc! Make connections and be encouraged!</p>
             </section>
